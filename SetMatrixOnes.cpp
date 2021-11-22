03cdf7d26b18f45706c91a6875caa64b46395178
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-22 15:12:39 +0530

    new

--- SetMatrixOnes.cpp
+++ SetMatrixOnes.cpp
@@ -1,28 +1,58 @@
-void setMatrixOnes(vector<vector<int>> &mat, int n, int m) // Brute force
+// Brute force  TC O(N*M+max(N,M))
+
+// void setMatrixOnes(vector<vector<int>> &mat, int n, int m) 
+// {
+//     // Write your code here.
+//     int row =n, col=m;
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             if(mat[i][j]==1){
+//                 // setting rows elements -1
+//                 for(int k=0;k<col;k++){
+//                     if(mat[i][k]==0)
+//                         mat[i][k]=-1;
+//                 }
+//                 // setting cols elements -1
+//                 for(int k=0;k<row;k++){
+//                     if(mat[k][j]==0)
+//                         mat[k][j]=-1;
+//                 }
+//             }
+//         }
+//     }
+    
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             if(mat[i][j]==-1)
+//                 mat[i][j]=1;
+//         }
+//     }
+// }
+
+
+
+// Optimized TC O(N*M) SC O(N+M)
+void setMatrixOnes(vector<vector<int>> &mat, int n, int m)
 {
     // Write your code here.
     int row =n, col=m;
+    vector<int> rowNumber (n,false);
+    vector<int> colNumber(m,false);
+    
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             if(mat[i][j]==1){
-                // setting rows elements -1
-                for(int k=0;k<col;k++){
-                    if(mat[i][k]==0)
-                        mat[i][k]=-1;
-                }
-                // setting cols elements -1
-                for(int k=0;k<row;k++){
-                    if(mat[k][j]==0)
-                        mat[k][j]=-1;
-                }
+                rowNumber[i]=true;
+                colNumber[j]=true;
             }
         }
     }
     
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            if(mat[i][j]==-1)
+            if(rowNumber[i]==true || colNumber[j]==true)
                 mat[i][j]=1;
         }
     }
 }
+
