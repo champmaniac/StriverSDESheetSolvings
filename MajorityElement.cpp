af94cb4f178cfc4549d1881a5dcde18c7c923c0f
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-24 18:55:29 +0530

    Day 3

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,28 +1,57 @@
-int findMajorityElement(int arr[], int n) {
-   int ele = arr[0], count=1;
-   for(int i=1;i<n;i++)
-   {
-   	if(arr[i]==ele)
-   		count++;
-    else
-    	count--;
-    if(count==0)
-   	{
-   		ele = arr[i];
-   		count=1;
-   	}
-   }
+// // Brute Force TC O(N^2) SC (1)
+// int findMajorityElement(int arr[], int n) {
+// 	for(int i=0;i<n;i++){
+// 		int maxCount=0;
+// 		for(int j=0;j<n;j++){
+// 			if(arr[i]==arr[j])
+// 				maxCount++;
+// 		}
+// 		if(maxCount>n/2)
+// 		return arr[i];
+// 	}
+// 	return -1;
+// }
 
-   	count =0;
-   	for(int i=0;i<n;i++){
-   		if(arr[i]==ele)
-   		{
-   			count++;
-   		}
-   	}
-   		
-   	if(count>n/2)
-   		return ele;
-   	return -1;
+// Optimized using Hashmap  TC O(N) SC O(N)
+int findMajorityElement(int arr[], int n) {
+	unordered_map<int,int> mp;
+	for(int i=0;i<n;i++)
+	{
+		mp[arr[i]]++;
+		if(mp[arr[i]]>n/2)
+			return arr[i];
+	}
+	return -1;
 }
+
+
+// Most Optimized  Boyer-Moore majority vote algo  TC O(N) SC O(1)
+
+// int findMajorityElement(int arr[], int n) {
+//    int ele = arr[0], count=1;
+//    for(int i=1;i<n;i++)
+//    {
+//    	if(arr[i]==ele)
+//    		count++;
+//     else
+//     	count--;
+//     if(count==0)
+//    	{
+//    		ele = arr[i];
+//    		count=1;
+//    	}
+//    }
+
+//    	count =0;
+//    	for(int i=0;i<n;i++){
+//    		if(arr[i]==ele)
+//    		{
+//    			count++;
+//    		}
+//    	}
+   		
+//    	if(count>n/2)
+//    		return ele;
+//    	return -1;
+// }
 
