548e4375f0d57111cce1a7a35558e54eae7c818b
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-24 19:58:20 +0530

    Day 3

--- MajorityElementII.cpp
+++ MajorityElementII.cpp
@@ -1,42 +1,42 @@
 // // Brute Force O(N^2)
-// vector<int> majorityElementII(vector<int> &arr)
-// {
-//     int n = arr.size();
-//     vector<int> ans;
-//     for(int i=0;i<n;i++)
-//     {
-//         int maxCount=1;
-//         for(int j=i+1;j<n;j++)
-//         {
-//             if(arr[i]==arr[j])
-//                 maxCount++;
-//         }
-//         if(maxCount>(n/3))
-//             ans.push_back(arr[i]);
-//     }
-//     return ans;
+vector<int> majorityElementII(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> ans;
+    for(int i=0;i<n;i++)
+    {
+        int maxCount=1;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+                maxCount++;
+        }
+        if(maxCount>(n/3))
+            ans.push_back(arr[i]);
+    }
+    return ans;
 
-// }
+}
 
 // Optimized using hashmap  TC O(N)  SC O(N)
 
-// #include <unordered_map>
-// vector<int> majorityElementII(vector<int> &arr)
-// {
-//     int n = arr.size();
-//     unordered_map<int,int> mp;
-//     vector<int> ans;
-//     for(int i=0;i<n;i++)
-//     {
-//         mp[arr[i]]++;
-//     }
-//     for(auto x:mp)
-//     {
-//         if(x.second>(n/3))
-//             ans.push_back(x.first);
-//     }
-//     return ans;
-// }
+#include <unordered_map>
+vector<int> majorityElementII(vector<int> &arr)
+{
+    int n = arr.size();
+    unordered_map<int,int> mp;
+    vector<int> ans;
+    for(int i=0;i<n;i++)
+    {
+        mp[arr[i]]++;
+    }
+    for(auto x:mp)
+    {
+        if(x.second>(n/3))
+            ans.push_back(x.first);
+    }
+    return ans;
+}
 
 // Best Optimized sol TC O(N) SC O(1)
 
