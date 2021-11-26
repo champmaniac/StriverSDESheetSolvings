4c225209a475a4375b3da39b2e4f870290b8172a
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-26 11:54:20 +0530

    Day 5

--- MiddleOfLinkedlist.cpp
+++ MiddleOfLinkedlist.cpp
@@ -1,4 +1,24 @@
-// Brute and the best way to solve is Two pointers approach
+// Brute Force 
+// TC O(N) SC O(N)
+Node *findMiddle(Node *head) {
+    Node* temp = head;
+    int n =0;
+    while(temp)
+    {
+        n++;
+        temp=temp->next;
+    }
+    temp = head;
+    for(int i=0;i<n/2;i++)
+    {
+        temp=temp->next;
+    }
+    return temp;
+}
+
+
+// Optimized way to solve is Two pointers approach 
+// Tortoise-Hare-Approach
 // TC O(N) SC O(1)
 Node *findMiddle(Node *head) {
     Node* slow = head, *fast =head;
