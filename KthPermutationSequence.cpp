327fe6a308787a9f4749f98864f2e9c6e5d50fc6
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-30 16:15:06 +0530

    Day 9

--- KthPermutationSequence.cpp
+++ KthPermutationSequence.cpp
@@ -1,3 +1,38 @@
+// Brute Force 
+// TC O(N!) SC O(N!)
+
+void permute(string s, string ans, vector<string> &res)
+{
+    if(s.size()==0)
+    {
+        res.push_back(ans);
+        return;
+    }
+
+    for(int i=0;i<s.size();i++)
+    {
+        ans+=s[i];
+
+        permute(s.substr(0,i)+s.substr(i+1),ans,res);
+
+        ans.pop_back();
+    }
+}
+
+string kthPermutation(int n, int k) {
+    vector<string> res;
+    string s="";
+
+    for(int i=1;i<=n;i++)
+    {
+        s+=to_string(i);
+    }
+    permute(s,"",res);
+    return res[k-1]; 
+}
+
+// Optimized approach
+// TC O(N^2) SC O(N)
 string kthPermutation(int n, int k) {
     // Write your code here.
     vector<int> number;
