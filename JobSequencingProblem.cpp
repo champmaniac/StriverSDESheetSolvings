7c7c8e8812367160210e16a8a68e8cde83e3ecc4
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-30 19:15:26 +0530

    Day 8

---
+++ JobSequencingProblem.cpp
@@ -0,0 +1,37 @@
+/*
+struct Job 
+{ 
+    int id;  // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+};
+*/
+
+// TC O(NlogN) SC O(N)
+class Solution 
+{
+    public:
+    static bool compare(Job a, Job b){
+        return a.profit>b.profit;
+    }
+    //Function to find the maximum profit and the number of jobs done.
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        // your code here
+        sort(arr,arr+n,compare);
+        int day =0,profit=0;
+        bool done[n]={0};
+        
+        for(int i=0;i<n;i++){
+            for(int j=min(n,arr[i].dead-1);j>=0;j--){
+                if(done[j]==false){
+                    day+=1;
+                    profit+=arr[i].profit;
+                    done[j]=true;
+                    break;
+                }
+            }
+        }
+        return {day,profit};
+    } 
+};
