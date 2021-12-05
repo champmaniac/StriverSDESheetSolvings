1dad6b95b66ce0223785a4ef184d07c42d3b8077
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-05 13:32:44 +0530

    Day 1

--- StocksAreProfitable.cpp
+++ StocksAreProfitable.cpp
@@ -1,10 +1,25 @@
-/*
-    Time Complexity: O(N)
-    Space Complexity: O(1)
+// Brute force
+// TC O(N^2) SC O(1)
+int maximumProfit(vector<int> &prices){
 
-    Where N is the size of the array.
-*/
+    int n = prices.size();
+    int maxProfit=0;
 
+    for(int i=0;i<n-1;i++)
+    {
+        int buy=prices[i], curMaxProfit =0; 
+
+        for(int j=i+1;j<n;j++)
+        {
+            curMaxProfit = max(curMaxProfit,(prices[j]-buy));
+        }
+        maxProfit= max(maxProfit,curMaxProfit);
+    }
+    return maxProfit;
+}
+
+// Optimized Brute force
+// TC O(N) SC O(1)
 int maximumProfit(vector<int> &prices){
 
     // Variables to store the buying price and maximum profit
