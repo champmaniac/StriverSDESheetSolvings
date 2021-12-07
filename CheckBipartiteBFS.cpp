b281da3ec1e634668d4ef24cc4863b19944d7ace
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-08 00:29:06 +0530

    Graphs

---
+++ CheckBipartiteBFS.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+bool isGraphBirpatite(vector<vector<int>> &edges) {
+	int n = edges.size();
+	vector<vector<int>> graph(n);
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(edges[i][j]){
+				graph[i].push_back(j);
+				graph[j].push_back(i);
+			}
+		}
+	}
+
+	vector<int> color (n,-1);
+
+	// marking the color of the root 0
+
+	for(int i=0;i<n;i++){
+		if(color[i]!=-1)
+		{
+			continue;
+		}
+		color[i]=0;
+		
+
+		queue<int> q;
+		q.push(i);
+
+		int c=0;
+		while(!q.empty()){
+			int node = q.front();
+			q.pop();
+
+			for(int nbr :graph[node]){
+				if(color[nbr]!=-1 && color[nbr]==color[node])
+					return false;
+				else if(color[nbr]==-1)
+				{
+					color[nbr]=!color[node];
+					q.push(nbr);
+				}
+			}
+			c=!c;
+		}
+	}
+	return true;
+}
