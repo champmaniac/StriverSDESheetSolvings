1926a97333b9152f0d5d7271262f15faf663a732
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-17 13:21:27 +0530

    Trees

---
+++ InorderTraversal.cpp
@@ -0,0 +1,18 @@
+// TC O(N) SC O(N)
+
+
+void dfs(TreeNode* root,vector<int> &inOrder){
+    if(root==NULL)
+        return;
+    dfs(root->left,inOrder);
+    inOrder.push_back(root->val);
+    dfs(root->right,inOrder);
+
+    
+}
+vector<int> preorderTraversal(TreeNode* root)
+{
+    vector<int> inOrder;
+    dfs(root,inOrder);
+    return inOrder;
+}
