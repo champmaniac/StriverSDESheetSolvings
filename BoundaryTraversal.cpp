506a22fea6ca502a528b365d8b087eb4de265c06
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-19 14:02:56 +0530

    Trees

---
+++ BoundaryTraversal.cpp
@@ -0,0 +1,51 @@
+class Solution{
+	bool isleaf(Node* root){
+		return !root->left && !root->right;
+	}
+
+	void addLeftBoundary(Node* root, vector<int> &res){
+		Node* curr = root->left;
+		if(!isleaf(curr)) res.push_back(curr->data);
+		if(curr->left) curr = curr->left;
+		else curr=curr->right;
+	}
+	// reverse right boundary
+	void addRightBoundary(Node* root, vector<int> &res){
+		Node* curr = root->right;
+		vector<int> st;
+		while(cur){
+			if(!isleaf(curr)) st.push_back(curr->data);
+			if(curr->right) curr=curr->right;
+			else curr = curr->right;
+		}
+
+		for(int i=st.size()-1;i>=0;--i){
+			res.push_back(temp[i]);
+		}
+	}
+
+	void addLeaves(Node* root,vector<int> &res){
+		if(isleaf(root)){
+			res.push_back(root->data);
+			return;
+		}
+		if(root->left) addLeaves(root->left,res);
+		if(root->right) addLeaves(root->right,res);
+	}
+
+
+public:
+	vector<int> printBoundary(Node* root){
+		vector<int> res;
+		if(!root) return res;
+
+		if(!isleaf(root)) res.push_back(root->data);
+		// adding left boundary
+		addLeftBoundary(root,res);
+		// add leaves
+		addLeaves(root,res);
+		// adding reversed right boundary
+		addRightBoundary(root,res);
+		return res;
+	}
+}
