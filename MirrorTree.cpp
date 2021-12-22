3a08c96f3b4a0b09973af2d4b949e247d1af001c
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-22 11:37:59 +0530

    Trees

---
+++ MirrorTree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        // code here
+        if(node==NULL) return;
+        Node* temp = node->left;
+        node->left=node->right;
+        node->right=temp;
+        
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
