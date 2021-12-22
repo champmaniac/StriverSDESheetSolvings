d1e92d09a4824b1cda81f12441e2f0cc7618bfcd
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-22 21:24:02 +0530

    Trees

---
+++ CeilFromBST.cpp
@@ -0,0 +1,20 @@
+int findCeil(BinaryTreeNode<int> *node, int x){
+    // Write your code here.
+    int ceil =-1;
+    while(node){
+        if(node->data==x)
+        {
+            ceil = node->data;
+            return ceil;
+        }
+        if(x>node->data){
+            node = node->right;
+        }
+        else
+        {
+            ceil = node->data;
+            node = node->left;
+        }
+    }
+    return ceil;
+}
