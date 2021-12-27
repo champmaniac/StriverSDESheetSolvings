edca24868f2ed04c67f783078636cc0db6104f33
Prantik Mukherjee <champmaniac04@gmail.com>
2021-12-28 00:31:25 +0530

    Trees

--- LCAOfBinaryTreeWith3Nodes.cpp
+++ LCAOfBinaryTreeWith3Nodes.cpp
@@ -1,3 +1,6 @@
+// Preorder Traversal
+// TC O(N) SC O(N)
+
 BinaryTreeNode<int>* lcaOfThreeNodes(BinaryTreeNode<int>* root, int p, int q, int r) {
     // Write your code here.
     if(root==NULL || root->data==p || root->data==q || root->data==r){
@@ -6,10 +9,10 @@ BinaryTreeNode<int>* lcaOfThreeNodes(BinaryTreeNode<int>* root, int p, int q, in
     BinaryTreeNode<int>* left = lcaOfThreeNodes(root->left,p,q,r);
     BinaryTreeNode<int>* right =lcaOfThreeNodes(root->right,p,q,r);
     
-    if(left==NULL)
-        return right;
-    if(right==NULL)
-        return left;
-    else
+    if(left!=NULL && right!=NULL) // both not null, then root
         return root;
+    if(left!=NULL) // only left, then return left
+        return left;
+    else // only right, then return right
+        return right;
 }
