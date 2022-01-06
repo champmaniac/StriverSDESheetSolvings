ac4e68209502bd284542400c4199608e7b3ca3e2
Prantik Mukherjee <champmaniac04@gmail.com>
2022-01-07 00:13:53 +0530

    Recursion

---
+++ Permutations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> perms;
+        permute(nums,0,perms);
+        return perms;
+    }
+    void permute(vector<int> nums,int i,vector<vector<int>> &perms){
+        if(i==nums.size())
+            perms.push_back(nums);
+        else{
+            for(int j=i;j<nums.size();j++){
+                swap(nums[i],nums[j]);
+                permute(nums,i+1,perms);
+            }
+        }
+    }
+};
