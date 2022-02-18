39a04534ebecfb09018a886451354894ae6c27d9
Prantik Mukherjee <champmaniac04@gmail.com>
2022-02-19 00:44:16 +0530

    updated

--- Sort012.cpp
+++ Sort012.cpp
@@ -1,22 +1,24 @@
 // Dutch national flag algo   TC O(N)  SC O(1)
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 void sort012(int *nums, int n)
 {
     int lo = 0;
-    int hi = n-1;
-    int mid=0;
-    while(mid<=hi){
-        switch(nums[mid]){
-            case 0:
-            	swap(nums[lo++],nums[mid++]);
-            	break;
-            case 1:
-            	mid++;
-            	break;
-            case 2:
-            	swap(nums[mid],nums[hi--]);
-            	break;
+    int hi = n - 1;
+    int mid = 0;
+    while (mid <= hi)
+    {
+        switch (nums[mid])
+        {
+        case 0:
+            swap(nums[lo++], nums[mid++]);
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(nums[mid], nums[hi--]);
+            break;
         }
     }
 }
