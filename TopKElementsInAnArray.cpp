a2fe07b0609bebaccecc2ebe7ad0cddc6b7752fd
Prantik Mukherjee <champmaniac04@gmail.com>
2022-02-21 12:27:39 +0530

    updated

---
+++ TopKElementsInAnArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> topK(vector<int>& nums, int k) {
+        // Code here
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        vector<pair<int,int>> v;
+        for(auto it:mp){
+            v.push_back({it.second,it.first});
+        }
+        sort(v.begin(),v.end());
+        vector<int> res;
+        int len = v.size();
+        for(int i=0;i<k;i++){
+            res.push_back(v[len-1].second);
+            len--;
+        }
+        return res;
+    }
+};
